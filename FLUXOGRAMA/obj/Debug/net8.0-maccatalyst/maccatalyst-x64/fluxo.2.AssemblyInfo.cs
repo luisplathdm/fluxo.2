401bc29735ba71447fa923b39c2021d3ba0ce647
hih <luisgustavoplathdisaro@gmail.com>
2024-08-19 11:52:31 -0300

    tudo errado

--- FLUXOGRAMA/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/fluxo.2.AssemblyInfo.cs
+++ FLUXOGRAMA/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/fluxo.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fluxo.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6318a1f2b3da146d3980fd7ec22fb119ab798d1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a0a92c6dfb2b592a2c4451a9082ed986415c6ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("fluxo.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fluxo.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
